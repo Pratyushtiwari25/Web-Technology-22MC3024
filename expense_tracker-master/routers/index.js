300a9e8af7f168903cbbd0470a14882877471009
Pratyush Tiwari <125774489+Pratyushtiwari25@users.noreply.github.com>
2024-04-28 05:50:45 +0530

    Delete expense_tracker-master directory

--- expense_tracker-master/routers/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const expenseRouter = require("./expenseRoutes");
-const userRouter = require("./userRoutes");
-const router = Router();
-
-router.use("/user", userRouter);
-router.use("/expense", expenseRouter);
-
-module.exports = router;
